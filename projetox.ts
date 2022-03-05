90c3413ef0421263f7bbd348d987a75602d5e5c6
Gabriel Viana <gsv.jesus@gmail.com>
2022-03-04 21:47:58 -0300

    aula 01

--- projetox.ts
+++ projetox.ts
@@ -2,8 +2,8 @@
 Faça um programa que receba o salário de um funcionário e o percentual de aumento, calcule e mostre o valor do aumento e o novo salário. Ex: funcionário ganha 1000 e vai receber 25% de aumento. no console.log deve mostrar 1250.*/
 
 var salario = 1000
-var aumento = 0,25
-var newSalario = salario*aumento
+var aumento = 0.25
+var newSalario = (salario*aumento)+salario
 console.log('O salário atual é',salario)
 console.log('O aumento será',aumento)
-console.log('o novo salário do funcionário será:',newSalario) 
+console.log('o novo salário do funcionário será:',newSalario)
