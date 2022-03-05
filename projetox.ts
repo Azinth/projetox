a841cbfaca2906b07f4a7ab2c8717a5438610870
Gabriel Viana <gsv.jesus@gmail.com>
2022-03-04 21:45:54 -0300

    exercício 1

--- projetox.ts
+++ projetox.ts
@@ -1,6 +1,9 @@
-// Aprendendo Git e GitHub
+/* atv01 
+Faça um programa que receba o salário de um funcionário e o percentual de aumento, calcule e mostre o valor do aumento e o novo salário. Ex: funcionário ganha 1000 e vai receber 25% de aumento. no console.log deve mostrar 1250.*/
 
-console.log("Gabriel Viana")
-console.log("Projeto Y")
-
-console.log("estou em uma nova branch")
+var salario = 1000
+var aumento = 0,25
+var newSalario = salario*aumento
+console.log('O salário atual é',salario)
+console.log('O aumento será',aumento)
+console.log('o novo salário do funcionário será:',newSalario) 
