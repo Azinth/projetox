aed18678f74eb4802a46ca000063d501beb3a4cb
Gabriel Viana <gsv.jesus@gmail.com>
2022-03-08 20:47:36 -0300

    fixed

--- projetox.ts
+++ projetox.ts
@@ -4,6 +4,6 @@ Faça um programa que receba o salário de um funcionário e o percentual de aum
 var salario = 1000
 var aumento = 0.25
 var newSalario = (salario*aumento)+salario
-console.log('O salário atual é'+salario)
-console.log('O aumento será'+(aumento*100)+'%')
-console.log('o novo salário do funcionário será:'+newSalario)
+console.log('O salário atual é '+salario)
+console.log('O aumento será de '+(aumento*100)+'%')
+console.log('o novo salário do funcionário será: '+newSalario)
